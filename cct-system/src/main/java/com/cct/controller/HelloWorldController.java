cdf0256d11dda440b4c3e0d4f2e1c2eea1ce16dd
Carlos Fernando Ruiz Nieto <carlos.ruiz@beitech.co>
2016-09-30 11:43:54 -0500

    Implementación JMS

--- cct-system/src/main/java/com/cct/controller/HelloWorldController.java
+++ cct-system/src/main/java/com/cct/controller/HelloWorldController.java
@@ -1,10 +1,16 @@
 package com.cct.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.cct.dto.Email;
 import com.cct.redis.RedisClient;
 
 @Controller
@@ -12,6 +18,9 @@ public class HelloWorldController {
 
 	@Autowired
 	private RedisClient redisClient;
+
+	@Autowired
+	private JmsTemplate jmsTemplate;
 
 	@RequestMapping("/")
 	@ResponseBody
@@ -28,6 +37,13 @@ public class HelloWorldController {
 		int realValue = Integer.parseInt((String) redisClient.get("key"));
 
 		return "Value: " + realValue;
+	}
+
+	@RequestMapping(value = "/message", method = RequestMethod.POST)
+	@ResponseBody
+	public ResponseEntity<String> sendMessage(@RequestBody Email email){
+		jmsTemplate.convertAndSend("mailbox", email);
+		return new ResponseEntity<>("Message Received!", HttpStatus.OK);
 	}
 
 }
