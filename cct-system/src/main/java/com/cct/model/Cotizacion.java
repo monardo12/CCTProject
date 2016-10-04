1fe16d57f7a0ff04c629b61b76698a7e302fee2f
Carlos Ruiz <carlos.ruiz@beitech.co>
2016-10-04 08:19:14 -0500

    Agregrar constructor, getters y setters Cotizacion

--- cct-system/src/main/java/com/cct/model/Cotizacion.java
+++ cct-system/src/main/java/com/cct/model/Cotizacion.java
@@ -14,53 +14,128 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 public class Cotizacion implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
 	@Id	
-	@Getter @Setter
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long idCotizacion;
 
-	@Getter @Setter
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="idServicio")
 	private Servicio servicio;
 
-	@Getter @Setter
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="idCliente")
 	private Cliente cliente;
 	
-	@Getter @Setter
     @JoinTable(name="item", joinColumns = {
             @JoinColumn(name = "idcotizacion", referencedColumnName = "idcotizacion")}, inverseJoinColumns = {
             @JoinColumn(name = "iditem", referencedColumnName = "iditem")})
 	@ManyToMany(fetch = FetchType.EAGER)
 	private Collection<Item> items;
 	
-	@Getter @Setter
 	private Date fechaCreacion;
 
-	@Getter @Setter
 	private Integer cantidadItem;
 		
-	@Getter @Setter
 	private String estado;
 
-	@Getter @Setter
 	private Double valorItem;
 	
-	@Getter @Setter
 	private Double total;
-	
+
+	public Cotizacion() {
+		
+	}
+
+	public Cotizacion(Long idCotizacion, Servicio servicio, Cliente cliente,
+			Collection<Item> items, Date fechaCreacion, Integer cantidadItem,
+			String estado, Double valorItem, Double total) {
+		super();
+		this.idCotizacion = idCotizacion;
+		this.servicio = servicio;
+		this.cliente = cliente;
+		this.items = items;
+		this.fechaCreacion = fechaCreacion;
+		this.cantidadItem = cantidadItem;
+		this.estado = estado;
+		this.valorItem = valorItem;
+		this.total = total;
+	}
+
+	public Long getIdCotizacion() {
+		return idCotizacion;
+	}
+
+	public void setIdCotizacion(Long idCotizacion) {
+		this.idCotizacion = idCotizacion;
+	}
+
+	public Servicio getServicio() {
+		return servicio;
+	}
+
+	public void setServicio(Servicio servicio) {
+		this.servicio = servicio;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Collection<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(Collection<Item> items) {
+		this.items = items;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public Integer getCantidadItem() {
+		return cantidadItem;
+	}
+
+	public void setCantidadItem(Integer cantidadItem) {
+		this.cantidadItem = cantidadItem;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Double getValorItem() {
+		return valorItem;
+	}
+
+	public void setValorItem(Double valorItem) {
+		this.valorItem = valorItem;
+	}
+
+	public Double getTotal() {
+		return total;
+	}
+
+	public void setTotal(Double total) {
+		this.total = total;
+	}
+
 }
