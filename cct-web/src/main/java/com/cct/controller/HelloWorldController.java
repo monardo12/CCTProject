84c33bfe6b5826cca7aef384a701fbf415c07939
Carlos Fernando Ruiz Nieto <monardo12@gmail.com>
2016-11-13 18:29:12 -0500

    Servicio POST

--- cct-web/src/main/java/com/cct/controller/HelloWorldController.java
+++ cct-web/src/main/java/com/cct/controller/HelloWorldController.java
@@ -1,8 +1,14 @@
 package com.cct.controller;
 
+import java.util.Arrays;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.cct.model.Cliente;
@@ -51,6 +57,20 @@ public class HelloWorldController {
 	@ResponseBody
 	public String loaderIOCarlos() {
 		return "loaderio-dc44d0b7c768d24ed2d9376d5ff1594b";
+	}
+	
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	@ResponseBody
+	public String form(HttpServletRequest request){
+		Map<String, String[]> params = request.getParameterMap();
+		StringBuilder response = new StringBuilder("Recibido: ");
+		for(Map.Entry<String, String[]> param : params.entrySet()){
+			response.append(param.getKey())
+			.append(":")
+			.append(Arrays.toString(param.getValue()))
+			.append(",");
+		}
+		return response.toString();
 	}
 
 }
