e8f8d34017c27f4aef2586595026ea01e4160e72
Yadiana Laiton <yadianalaiton@gmail.com>
2016-11-13 12:54:26 -0500

    Servicio crear usuarios.

---
+++ cct-common/src/main/java/com/cct/dto/UsuarioDTO.java
@@ -0,0 +1,75 @@
+package com.cct.dto;
+
+public class UsuarioDTO {
+	
+	private Long idUsuario;
+
+	private String email;
+
+	private String nombre;
+	
+	private String username;
+	
+	private String password;
+	
+	private String privateKey;
+	
+	private String publicKey;
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPrivateKey() {
+		return privateKey;
+	}
+
+	public void setPrivateKey(String privateKey) {
+		this.privateKey = privateKey;
+	}
+
+	public String getPublicKey() {
+		return publicKey;
+	}
+
+	public void setPublicKey(String publicKey) {
+		this.publicKey = publicKey;
+	}
+
+}
